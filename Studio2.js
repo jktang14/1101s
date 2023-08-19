70052b0728c36788e4aee34e07f043058cc9ff8a
Source Academy User <No public email provided>
2023-08-19 21:23:12 +0800

    Changes made from Source Academy

--- Studio2.js
+++ Studio2.js
@@ -1,39 +1,26 @@
 function biggie_size(combo) {
-    return combo === 1 
-    ? 5 
-    :combo === 2 
-    ? 6 
-    :combo === 3
-    ? 7 
-    : 8;
+    return combo + 4;
 }
 
-biggie_size(2);
+display(biggie_size(2));
 
 function unbiggie_size(combo) {
-    return combo === 5 ? 1 : combo === 6 
-    ? 2 
-    : combo === 7
-    ? 3
-    : 4;
+    return combo - 4;
 } 
 
-unbiggie_size(7);
+display(unbiggie_size(7));
 
 function is_biggie_size(combo) {
-    /*return combo === 5 ? true : combo === 6 
-    ? true
-    : combo === 7
-    ? true
-    : combo === 8;*/
-    return combo === 5 || combo === 6 || combo === 7 || combo === 8;
+    return combo > 4;
 }
 
-is_biggie_size(8);
+display(is_biggie_size(4));
 
 function combo_price(combo) {
-    
+    return is_biggie_size(combo) ? unbiggie_size(combo) * 1.17 + 0.5 : combo * 1.17;
 }
+
+display(combo_price(5));
 
 
 
