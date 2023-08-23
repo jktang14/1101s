b571e25f538869448850ad4b82f1389cc5625845
Source Academy User <No public email provided>
2023-08-23 17:46:38 +0800

    Changes made from Source Academy

--- Reflection2.js
+++ Reflection2.js
@@ -1,22 +1,25 @@
-// q1
-/*Recursive process, when factorial(n - 1) is returned, 
-still needs to be multiplied by n, there is a deferred operation */
+/*q1
+Recursive process, when factorial(n - 1) is returned, 
+still needs to be multiplied by n, there is a deferred operation
 
-// q2
-// When there is a change in value, when there is a new expression being evaluated
+q2
+When there is a change in value, when there is a new expression being evaluated
 
-// q3
-// 6 steps
+q3
+6 steps
 
-//q4
-//6 deferred operations
+q4
+6 deferred operations
 
-//q5
-/* iter(1, 1, 5)
+q5
+iter(1, 1, 5)
 iter (5, 2, 5)
 iter(10, 3, 5)
 iter(30, 4, 5)
 iter(120, 5, 5)
 return product
 5 steps
+
+q6 
+No deferred operations
 
