14becb90feb127a2cad88752503a986275f7a850
Source Academy User <No public email provided>
2023-08-23 17:44:55 +0800

    Changes made from Source Academy

--- Reflection2.js
+++ Reflection2.js
@@ -3,10 +3,20 @@
 still needs to be multiplied by n, there is a deferred operation */
 
 // q2
-
+// When there is a change in value, when there is a new expression being evaluated
 
 // q3
 // 6 steps
 
 //q4
-//3 deferred operations
+//6 deferred operations
+
+//q5
+/* iter(1, 1, 5)
+iter (5, 2, 5)
+iter(10, 3, 5)
+iter(30, 4, 5)
+iter(120, 5, 5)
+return product
+5 steps
+
