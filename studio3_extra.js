b75eabc6c7a6ccdff9cb0013a2421f2ac877d947
Source Academy User <No public email provided>
2023-08-29 20:14:43 +0800

    Changes made from Source Academy

--- studio3_extra.js
+++ studio3_extra.js
@@ -4,3 +4,12 @@ Time: Θ(n) as n increases
 Space: Θ(n) as n increases
 
 Θ(1) for both space and time as b grows
+
+*/
+
+function fast_expt(b, n) {
+    return n === 1
+    ? b
+    : n % 2 === 0
+    ? 
+}
