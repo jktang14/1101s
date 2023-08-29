7380e3bb31afce1d6026e7f5ddc92067b312ec64
Source Academy User <No public email provided>
2023-08-29 21:13:49 +0800

    Changes made from Source Academy

--- studio3_extra.js
+++ studio3_extra.js
@@ -22,15 +22,22 @@ function fast_expt(b, n) {
 fast_expt(3, 2);
 
 function iter_fast_expt(b, n) {
-    return iter(n, b, b);
+    return iter(n, b, 1);
 }
 
-function iter(expt, b, counter) {
+function iter(expt, b, product) {
     return expt === 0
-    ? 1
+    ? product
     : expt % 2 === 0
-    ? iter(expt / 2, b, counter * counter)
+    ? iter(expt / 2, b * b, product)
+    : iter(expt - 1, b, product * b);
 }
 
+/*iter(3, 4, 1)
+iter(2, 4, 4)
+iter(1, 16, 4)
+iter(0, 16, 16 * 4)
+*/
 
+iter_fast_expt(4, 3);
 
