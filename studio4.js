de635663bee7376f3c50376a9cdf83d1ea6e90a6
Source Academy User <No public email provided>
2023-09-03 22:41:02 +0800

    Changes made from Source Academy

--- studio4.js
+++ studio4.js
@@ -2,4 +2,14 @@ function pascal(row, position) {
     if (row === 0 || position === 0 || position === row) {
         return 1;
     }
+    else {
+        return pascal(row - 1, position - 1) + pascal(row - 1, position);
+    }
 }
+display(pascal(0,0));
+display(pascal(1, 0));
+display(pascal(1,1));
+display(pascal(2,1));
+display(pascal(3,1));
+display(pascal(3,2));
+display(pascal(4,2));
