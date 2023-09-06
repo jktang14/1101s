b42bff83e2b167b5d33543b67bb9ffada450745d
Source Academy User <No public email provided>
2023-09-06 16:46:54 +0800

    Changes made from Source Academy

--- reflection4.js
+++ reflection4.js
@@ -2,4 +2,12 @@
 
 // q2
 // a
-pair(1, pair(2, pair(3, null)));
+display(pair(1, pair(2, pair(3, null))));
+
+// b
+display(pair(1, pair(2, 3)));
+
+// c
+display(pair(pair(1, pair(2, null)), list(pair(3, pair(4,null)), pair(5, pair(6, null)))));
+
+display(list(1,2));
