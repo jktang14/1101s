1461ef880096e0dc8cda1f92a75f8ce4d5552fd9
Source Academy User <No public email provided>
2023-09-11 12:04:08 +0800

    Changes made from Source Academy

--- studio5.js
+++ studio5.js
@@ -1,8 +1,7 @@
-function reverse(lst) {
-return is_null(lst)
-? null
-: list(reverse(tail(lst)), head(lst));
-}
+const lst1 = list(7, list(6, 5, 4), 3, list(2, 1));
+display(head(tail(head(tail(tail(tail(lst1)))))));
 
-reverse(list(1, 2, 3, 4));
+const lst2 = ist(list(7), list(6, 5, 4), list(3, 2), 1);
+const lst3 = list(7, list(6), list(5, list(4)), list(3, list(2, list(1))));
+const lst4 = list(7, list(list(6, 5), list(4), 3, 2), list(list(1)));
 
