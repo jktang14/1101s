c1162b38b9776f27f34f646fd30bfb12c744dc7f
Source Academy User <No public email provided>
2023-09-13 17:40:41 +0800

    Changes made from Source Academy

--- reflection5.js
+++ reflection5.js
@@ -9,13 +9,16 @@ flatten_list(LoL);
 
 // q2
 function tree_sum(tree) {
-    if is_null(tree) {
+    if (is_null(tree)) {
         return 0;
     }
-    else if is_list(head(tree)) {
+    else if (is_list(head(tree))) {
         return tree_sum(head(tree)) + tree_sum(tail(tree));
     }
     else {
         return head(tree) + tree_sum(tail(tree));
     }
 }
+
+const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
+tree_sum(my_tree);
