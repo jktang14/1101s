98e79ba7a4a2fcd1f02fcae720f0f5abb2222811
Source Academy User <No public email provided>
2023-09-13 17:53:19 +0800

    Changes made from Source Academy

--- reflection5.js
+++ reflection5.js
@@ -22,3 +22,7 @@ function tree_sum(tree) {
 
 const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
 tree_sum(my_tree);
+
+function accumulate_tree(f, op, initial, tree) {
+    return accumulate()
+}
