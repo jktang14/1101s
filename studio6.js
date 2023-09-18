5d44c82bc6a379e1be6f94596dfa61d15abdf729
Source Academy User <No public email provided>
2023-09-18 12:48:19 +0800

    Changes made from Source Academy

--- studio6.js
+++ studio6.js
@@ -19,3 +19,35 @@ display(remove_duplicates(list("a", "x", "b", "c", "c", "b", "d")));
 // Result: list("a", "x", "b", "c", "d")
 
 // q3
+function makeup_amount(x, coins) {
+    if (x === 0) {
+        // 1 way, sublist with null value
+        return list(null);
+    } 
+    else if (x < 0 || is_null(coins)) {
+        return null;
+    } 
+    else {
+        // Combinations that do not use the head coin.
+        const combi_A = ...
+        // Combinations that do not use the head coin
+        // for the remaining amount.
+        const combi_B = ...
+        // Combinations that use the head coin.
+        const combi_C = ...
+        return append(combi_A, combi_C);
+    }
+}
+
+makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50));
+// Result: list(list(20, 1, 1), list(10, 5, 1, 5, 1), list(1, 20, 1),
+// list(1, 20, 1), list(1, 10, 5, 5, 1),
+// list(1, 10, 5, 1, 5))
+
+
+
+
+
+
+
+
