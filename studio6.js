023e2bb88e5a8c6b9f05701c4b378b4003c064dd
Source Academy User <No public email provided>
2023-09-18 12:42:12 +0800

    Changes made from Source Academy

--- studio6.js
+++ studio6.js
@@ -3,13 +3,14 @@ function my_map(f, xs) {
     return accumulate((x, y) => append(list(f(x)), y), null, xs);
 }
 
-display(my_map(x => x + 1, list(1, 2, 3)));
+// display(my_map(x => x + 1, list(1, 2, 3)));
 
 // q2
 function remove_duplicates(xs) {
     return is_null(xs)
     ? null
-    : append(list(head(xs)), filter(x => x === head(xs), remove_duplicates(tail(xs))));
+    : 
+    append(list(head(xs)), filter(x => x !== head(xs), remove_duplicates(tail(xs))));
 }
 
 remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2));
