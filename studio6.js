c4b22461414c75a8aa4ca925b3b48494db19b1a4
Source Academy User <No public email provided>
2023-09-18 12:28:36 +0800

    Changes made from Source Academy

--- studio6.js
+++ studio6.js
@@ -4,3 +4,13 @@ function my_map(f, xs) {
 }
 
 my_map(x => x + 1, list(1, 2, 3));
+
+// q2
+function remove_duplicates(xs) {
+    return is_null(xs)
+    ? null
+    : return append(list(head(xs)), filter(x => x === head(xs), remove_duplicates(tail(xs))));
+}
+
+remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2));
+// Result: list(1, 2, 3, 4)
