dba65bc09847c937706c9deb9dfc9fa41547de20
Source Academy User <No public email provided>
2023-09-21 01:04:08 +0800

    Changes made from Source Academy

--- reflection6.js
+++ reflection6.js
@@ -1,3 +1,4 @@
+// xs is a list that has already been sorted
 function insert_cmp(x, xs, cmp) {
     return is_null(xs) 
            ? list(x)
@@ -26,9 +27,9 @@ const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 // Result: list(9, 8, 7, 6, 6, 5, 4, 3, 2, 1)
 
 // (c)
- insertion_sort_cmp(xs, );
+// insertion_sort_cmp(xs, (x, y) => false);
 // Result: list(7, 2, 4, 6, 9, 1, 5, 8, 3, 6)
 
 // (d)
-// insertion_sort_cmp(xs, ???);
+insertion_sort_cmp(xs, (x, y) => x < y);
 // Result: list(2, 4, 6, 6, 8, 9, 7, 5, 3, 1)
