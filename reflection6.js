6437a3b0d64968320ae58491ea1adbc63e7022e4
Source Academy User <No public email provided>
2023-09-21 15:32:18 +0800

    Changes made from Source Academy

--- reflection6.js
+++ reflection6.js
@@ -37,7 +37,7 @@ const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 // Result: list(7, 2, 4, 6, 9, 1, 5, 8, 3, 6)
 
 // (d)
-insertion_sort_cmp(xs, (x, y) => x % 2 === 0 
+// insertion_sort_cmp(xs, (x, y) => x % 2 === 0 
                         ? (y % 2 === 0 ? x <= y : true)
                         : (y % 2 === 0 ? false : x > y));
 // 6 and 2, 4, 6, 8, 9, 7, 5, 3, 1
