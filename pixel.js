5fb35aaf71494b2e88dc8680041e44c409fd9db2
Source Academy User <No public email provided>
2023-09-28 21:18:00 +0800

    Changes made from Source Academy

---
+++ pixel.js
@@ -0,0 +1,39 @@
+// Level 1
+function count_grantable_wishes(wishlist, skill_level) {
+    if (is_null(wishlist)) {
+        return 0;
+    }
+    else {
+        const curr_skill = tail(head(wishlist));
+        if (curr_skill <= skill_level) {
+            return 1 + count_grantable_wishes(tail(wishlist), skill_level);
+        }
+        else {
+            return count_grantable_wishes(tail(wishlist), skill_level);
+        }
+    }
+}
+
+const wishlist = list(pair("b", 2), pair("c", 3), pair("a", 1));
+count_grantable_wishes(wishlist, 2);
+
+function biggest_grantable_wish(wishlist, skill_level) {
+    if (is_null(wishlist)) {
+        return pair("", 0);
+    }
+    else {
+        const curr_score = tail(head(wishlist));
+        const next_biggest = biggest_grantable_wish(tail(wishlist), skill_level);
+        if (curr_score <= skill_level) {
+            if (curr_score > tail(next_biggest)) {
+                return head(wishlist);
+            }
+            else {
+                return next_biggest;
+            }
+        }
+        else {
+            
+        }
+    }
+}
