df9d4dbca905990972e4190ef53c9c84227b14e9
Source Academy User <No public email provided>
2023-10-16 12:42:30 +0800

    Changes made from Source Academy

--- studio8.js
+++ studio8.js
@@ -1,17 +1,6 @@
-//1 + (2 + (3 + (4 + (5 + 6)))); // control: 7, stash: 6
-// ((((1 + 2) + 3) + 4) + 5) + 6;
-//(1 + 2 + 3) + (4 + 5 + 6);
-//(1 + 2) + (3 + 4) + (5 + 6);
-
-// q2
-/* const c = 1;
-let x = c + 2;
-const d = c + x;
-let y = 3;
-x = c;
-y = d; */
-
-let x = undefined;
-for (x = 0; x < 10; x = x + 2) {
-x;
+function change(x, new_value) {
+x = new_value;
 }
+Consider the following statements:
+let x = 0;
+change(x, 1);
