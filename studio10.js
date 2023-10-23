c13361bfdf3a2a32f71c48f9aae2ba494c14d82d
Source Academy User <No public email provided>
2023-10-23 13:22:24 +0800

    Changes made from Source Academy

--- studio10.js
+++ studio10.js
@@ -19,5 +19,25 @@ const arr = [1, 2, 3, 4, 5];
 reverse_array(arr);
 arr;
 
-// q2
+// q2 a
 // theta (n^2)
+
+// q2 b
+function bubblesort_list(lst) {
+    const len = length(lst);
+    for (let i = len - 1; i > 0; i = i - 1) {
+        for (let j = 0; j < i; j = j + 1) {
+            const first = list_ref(lst, j);
+            const second = list_ref(lst, j + 1);
+            if (first < second) {
+                set_head(member(second, lst), first);
+                set_head(lst, list_ref(lst, j + 1));
+                
+            }
+        }
+    }
+}
+const LL = list(3, 5, 2, 4, 1);
+// 1 2 4 5
+bubblesort_list(LL);
+LL; // should show [1, [2, [3, [4, [5, null]]]]]
