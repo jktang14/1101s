d8e35b73fb9437a4f139599c17c3ec1f07ad4ddd
Source Academy User <No public email provided>
2023-10-23 12:41:38 +0800

    Changes made from Source Academy

---
+++ studio10.js
@@ -0,0 +1,20 @@
+function swap(A, i, j) {
+    let temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+
+function reverse_array(A) {
+    const len = array_length(A);
+    const half_len = math_floor(len / 2);
+    let i = 0;
+    while (i < half_len) {
+        const j = len - 1 - i;
+        swap(A, i, j);
+        i = i + 1;
+    }
+}
+
+const arr = [1, 2, 3, 4, 5];
+reverse_array(arr);
+arr;
