a468088c511489d021bf1f25363fe366af59d0c3
Source Academy User <No public email provided>
2023-10-23 15:15:48 +0800

    Changes made from Source Academy

--- studio10_extra.js
+++ studio10_extra.js
@@ -1,1 +1,15 @@
-member(2, list(1,2,3,4));
+function rotate_matrix(M) {
+    function swap(M, row, column) {
+        const temp = M[column][row];
+        M[column][row] = M[row][column];
+    }
+    const len = array_length(M);
+    for (let i = 0; i < len; i + 1) {
+        for (let j = 1; j < len; j + 1) {
+            swap(M, j, i);
+        }
+    }
+}
+
+// 30 20 10 00
+// 00 01 02 03
