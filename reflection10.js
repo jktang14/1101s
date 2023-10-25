22395c76d78c3127853ba5141e6a5eaad7327a1e
Source Academy User <No public email provided>
2023-10-25 20:50:19 +0800

    Changes made from Source Academy

---
+++ reflection10.js
@@ -0,0 +1,42 @@
+function memo_fun(fun) {
+    let already_run = false;
+    let result = undefined;
+
+    function mfun() {
+        if (!already_run) {
+            result = fun();
+            already_run = true;
+            return result;
+        } else {
+            return result;
+        }
+    }
+    return mfun;
+}
+
+function add_streams(s1, s2) {
+    return is_null(s1)
+        ? s2
+        : is_null(s2)
+        ? s1
+        : pair(head(s1) + head(s2),
+               () => add_streams(stream_tail(s1), 
+                                 stream_tail(s2)));
+}
+
+function mul_streams(s1, s2) {
+    return is_null(s1)
+        ? s2
+        : is_null(s2)
+        ? s1
+        : pair(head(s1) * head(s2),
+               () => mul_streams(stream_tail(s1), 
+                                 stream_tail(s2)));
+}
+
+function scale_stream(s, f) {
+    return stream_map(x => x * f, s);
+}
+
+const integers = integers_from(1);
+const ones = pair(1, () => ones);
