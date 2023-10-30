aaeeeb791c4a78295c8d01dbc6c76b7e99a4bc4c
Source Academy User <No public email provided>
2023-10-30 12:28:53 +0800

    Changes made from Source Academy

--- Studio11.js
+++ Studio11.js
@@ -3,5 +3,15 @@ function scale_stream(c, stream) {
     return stream_map(x => c * x, stream);
 }
 const A = pair(1, () => scale_stream(2, A));
-/* First value of A is 1, 
-*/
+display(stream_tail(A));
+eval_stream(A, 3);
+/* First value of A is 1, 2nd value is 2, 4 and so on
+*/ 
+
+// Assuming integers is a stream of positive integers
+function mul_streams(a,b) {
+    return pair(head(a) * head(b),
+            () => mul_streams(stream_tail(a), stream_tail(b)));
+}
+
+const B = pair(1, () => mul_streams(B, integers));
