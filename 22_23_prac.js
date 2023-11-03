6a04e043ff0c79b85c48ca40639230f016fe8e8c
Source Academy User <No public email provided>
2023-11-03 23:44:19 +0800

    Changes made from Source Academy

--- 22_23_prac.js
+++ 22_23_prac.js
@@ -130,8 +130,8 @@ function make_NiFT(T) {
     return append(number_lst, tree);
 }
 
-const tree1 = list( list(4,4,1), 5, 2, list(8,9), 4, list(6,7), 3 );
-display_list(make_NiFT(tree1));
+//const tree1 = list( list(4,4,1), 5, 2, list(8,9), 4, list(6,7), 3 );
+//display_list(make_NiFT(tree1));
 // returns list( 5, 2, 4, 3, list(4,4,1), list(8,9), list(6,7) )
 //const tree2 = list( list(1, 2, list(3, 4), 5), 6, null, list(7), 8, 9,
 //list( list(10), 11, list(12, 13, list(14, 15)) ) );
@@ -139,6 +139,9 @@ display_list(make_NiFT(tree1));
 // returns list( 6, 8, 9, list(1, 2, 5, list(3, 4)), null, list(7),
 // list( 11, list(10), list(12, 13, list(14, 15)) ) )
 
+function make_SToN(T) {
+    
+}
 
 
 
